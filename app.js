479865aa119feb66a11a5a8f5717384c6b70694a
NathanSt10 <nstout@uwm.edu>
2024-04-11 17:20:55 -0500

    Changed button ordering, added more (nonfunctional) buttons

--- app.js
+++ app.js
@@ -1,74 +1,55 @@
 var x = y = operator = null;
 var isEvaluated = false;
 const calculator = document.querySelector("#calculator");
-const buttons = document.querySelector("#buttons");
+const numbers = document.querySelector("#numbers");
+const operators = document.querySelector("#operators");
 const screen = document.querySelector("#screen");
 
-for (let i = 9; i >= 0; --i) {
-    const button = document.createElement("button");
-    button.textContent = i;
-    button.addEventListener("click", () => {populate(i);
-        console.log("----" + i + "----");
-        console.log("x: " + x);
-        console.log("y: " + y);
-        console.log("isEvaluated: " + isEvaluated);
-        console.log("--------------");});
-    buttons.appendChild(button);
-}
-const addition = document.createElement("button");
-addition.textContent = "+";
-addition.addEventListener("click", () => {if (x !== null && (operator === null || isEvaluated)) operator = add; if (y !== null) operator();
-    console.log("----addition----");
-    console.log("x: " + x);
-    console.log("y: " + y);
-    console.log("isEvaluated: " + isEvaluated);
-    console.log("--------------");});
-buttons.appendChild(addition);
+const dot = document.createElement("button");
+dot.textContent = ".";
+dot.addEventListener("click", () => {x = y = operator = null; screen.textContent = ""});
+numbers.appendChild(dot);
 
-const subtraction = document.createElement("button");
-subtraction.textContent = "-";
-subtraction.addEventListener("click", () => {if (x !== null && (operator === null || isEvaluated)) operator = subtract; if (y !== null) operator();
-    console.log("----subtraction----");
-    console.log("x: " + x);
-    console.log("y: " + y);
-    console.log("isEvaluated: " + isEvaluated);
-    console.log("--------------");});
-buttons.appendChild(subtraction);
-
-const multiplication = document.createElement("button");
-multiplication.textContent = "x";
-multiplication.addEventListener("click", () => {if (x !== null && (operator === null || isEvaluated)) operator = multiply; if (y !== null) operator();
-    console.log("----multiplication---");
-    console.log("x: " + x);
-    console.log("y: " + y);
-    console.log("isEvaluated: " + isEvaluated);
-    console.log("--------------");});
-buttons.appendChild(multiplication);
-
-const division = document.createElement("button");
-division.textContent = "÷";
-division.addEventListener("click", () => {if (x !== null && (operator === null || isEvaluated)) operator = divide; if (y !== null) operator();
-    console.log("----division----");
-    console.log("x: " + x);
-    console.log("y: " + y);
-    console.log("isEvaluated: " + isEvaluated);
-    console.log("--------------");});
-buttons.appendChild(division);
-
-const equals = document.createElement("button");
-equals.textContent = "=";
-equals.addEventListener("click", () => {if (y != null) operator(true);
-    console.log("------equals------");
-    console.log("x: " + x);
-    console.log("y: " + y);
-    console.log("isEvaluated: " + isEvaluated);
-    console.log("--------------");});
-buttons.appendChild(equals);
+const back = document.createElement("button");
+back.textContent = "<";
+back.addEventListener("click", () => {x = y = operator = null; screen.textContent = ""});
+numbers.appendChild(back);
 
 const clear = document.createElement("button");
 clear.textContent = "c";
 clear.addEventListener("click", () => {x = y = operator = null; screen.textContent = ""});
-buttons.appendChild(clear);
+numbers.appendChild(clear);
+
+for (let i = 9; i >= 0; --i) {
+    const button = document.createElement("button");
+    button.textContent = i;
+    button.addEventListener("click", () => {populate(i);});
+    numbers.appendChild(button);
+}
+const addition = document.createElement("button");
+addition.textContent = "+";
+addition.addEventListener("click", () => {if (x !== null && (operator === null || isEvaluated)) operator = add; if (y !== null) operator();});
+operators.appendChild(addition);
+
+const subtraction = document.createElement("button");
+subtraction.textContent = "-";
+subtraction.addEventListener("click", () => {if (x !== null && (operator === null || isEvaluated)) operator = subtract; if (y !== null) operator();});
+operators.appendChild(subtraction);
+
+const multiplication = document.createElement("button");
+multiplication.textContent = "x";
+multiplication.addEventListener("click", () => {if (x !== null && (operator === null || isEvaluated)) operator = multiply; if (y !== null) operator();});
+operators.appendChild(multiplication);
+
+const division = document.createElement("button");
+division.textContent = "÷";
+division.addEventListener("click", () => {if (x !== null && (operator === null || isEvaluated)) operator = divide; if (y !== null) operator();});
+operators.appendChild(division);
+
+const equals = document.createElement("button");
+equals.textContent = "=";
+equals.addEventListener("click", () => {if (y != null) operator(true);});
+operators.appendChild(equals);
 
 function populate(i) {
     let numStr = i.toString();
@@ -100,11 +81,6 @@ function add(evaluated = false) {
     isEvaluated = evaluated;
     if (isEvaluated) operator = null;
     screen.textContent = x;
-    console.log("------add------");
-    console.log("x: " + x);
-    console.log("y: " + y);
-    console.log("isEvaluated: " + isEvaluated);
-    console.log("--------------");
 }
 
 function subtract(evaluated = false) {
@@ -116,11 +92,6 @@ function subtract(evaluated = false) {
     isEvaluated = evaluated;
     if (isEvaluated) operator = null;
     screen.textContent = x;
-    console.log("------sub------")
-    console.log("x: " + x);
-    console.log("y: " + y);
-    console.log("isEvaluated: " + isEvaluated);
-    console.log("--------------")
 }
 
 function multiply(evaluated = false) {
@@ -132,11 +103,6 @@ function multiply(evaluated = false) {
     isEvaluated = evaluated;
     if (isEvaluated) operator = null;
     screen.textContent = x;
-    console.log("------mul------")
-    console.log("x: " + x);
-    console.log("y: " + y);
-    console.log("isEvaluated: " + isEvaluated);
-    console.log("--------------")
 }
 
 function divide(evaluated = false) {
@@ -154,9 +120,4 @@ function divide(evaluated = false) {
     isEvaluated = evaluated;
     if (isEvaluated) operator = null;
     screen.textContent = x;
-    console.log("------div------")
-    console.log("x: " + x);
-    console.log("y: " + y);
-    console.log("isEvaluated: " + isEvaluated);
-    console.log("--------------")
 }
